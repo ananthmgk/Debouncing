ff364679b2a98e3824b1a966927ed59345de6cd7
ananthmgk <ananthmgk4002@gmail.com>
2023-12-26 22:03:46 +0530

    updated

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
+let searchFetch = document.getElementById("fetching-api");
+// let delayInput = document.getElementById("delay-entry").value;
+let searchInput = document.getElementById("search");
 let count = 0;
 const clickFunc = () => {
   count += 1;
-  console.log("Debounce click" + " " + count);
+  searchFetch.innerText = `Fetching Data : ${count}`;
+  // console.log(delayInput);
+  // console.log(e.target.value);
 };
 
 function debounceing(func) {
   let timeout;
-  let count = 0;
   return function (args) {
-    count += 1;
-    console.log("nonDebounce click" + " " + count);
+    const inputValue = document.getElementById("delay-entry").value;
     clearTimeout(timeout);
     timeout = setTimeout(() => {
       func(args);
-    }, 500);
+    }, inputValue);
   };
 }
 
 const starter = debounceing(clickFunc);
+// const processInput = (event) => {
+//   if (event.key === "Enter") {
+//     console.log("You entered:", delayInput);
+//   } else {
+//     console.log("You entered:", delayInput);
+//   }
+// };
+
+function processInput() {
+  const inputValue = document.getElementById("delay-entry").value;
+  return inputValue;
+  // console.log("You entered:", inputValue);
+}
+
+searchInput.addEventListener("change", starter);
+searchInput.addEventListener("keyup", starter);
+
+// onkeyup = "if (event.key === 'Enter') processInput()";
