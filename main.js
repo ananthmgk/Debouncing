69e19e20b7e2115f084d57eb2a9b946b318ef0be
ananthmgk <ananthmgk4002@gmail.com>
2023-12-29 02:44:13 +0530

    mingled two functions

--- main.js
+++ main.js
@@ -1,41 +1,56 @@
-let searchFetch = document.getElementById("fetching-api");
-// let delayInput = document.getElementById("delay-entry").value;
-let searchInput = document.getElementById("search");
+const fetchSearch = document.querySelector(".fetching-api");
+const debounceSearch = document.querySelector(".debounce-search");
+const throttleSearch = document.querySelector(".throttle-search");
+const title = document.querySelector(".title");
 let count = 0;
-const clickFunc = () => {
+
+function toggleBtn() {
+  if (title.innerText === "Debouncing") {
+    title.innerText = "Throttling";
+    throttleSearch.style.display = "block";
+    debounceSearch.style.display = "none";
+    debounceSearch.value = "";
+  } else {
+    title.innerText = "Debouncing";
+    throttleSearch.style.display = "none";
+    debounceSearch.style.display = "block";
+    throttleSearch.value = "";
+  }
+  count = 0;
+}
+
+const countFunc = () => {
   count += 1;
-  searchFetch.innerText = `Fetching Data : ${count}`;
-  // console.log(delayInput);
-  // console.log(e.target.value);
+  fetchSearch.innerText = `Fetching Data : ${count}`;
 };
 
 function debounceing(func) {
   let timeout;
-  return function (args) {
-    const inputValue = document.getElementById("delay-entry").value;
+  return function (...args) {
+    const delayValue = document.getElementById("delay-entry").value;
     clearTimeout(timeout);
     timeout = setTimeout(() => {
       func(args);
-    }, inputValue);
+    }, delayValue);
   };
 }
 
-const starter = debounceing(clickFunc);
-// const processInput = (event) => {
-//   if (event.key === "Enter") {
-//     console.log("You entered:", delayInput);
-//   } else {
-//     console.log("You entered:", delayInput);
-//   }
-// };
+function throttle(func) {
+  let shouldWait = false;
+  return (...args) => {
+    const delayValue = document.getElementById("delay-entry").value;
+    if (shouldWait) return;
 
-function processInput() {
-  const inputValue = document.getElementById("delay-entry").value;
-  return inputValue;
-  // console.log("You entered:", inputValue);
+    func(...args);
+    shouldWait = true;
+    setTimeout(() => {
+      shouldWait = false;
+    }, delayValue);
+  };
 }
 
-searchInput.addEventListener("change", starter);
-searchInput.addEventListener("keyup", starter);
+const starterDebounce = debounceing(countFunc);
+const starterThrottle = throttle(countFunc);
 
-// onkeyup = "if (event.key === 'Enter') processInput()";
+debounceSearch.addEventListener("keyup", starterDebounce);
+throttleSearch.addEventListener("keyup", starterThrottle);
